7ead6b2e071ef72b384484e9935e649402f2c7ff
fedegiuliano <fedegiuliano11.com>
2019-05-19 19:40:28 -0300

    Realizando el ejercicio numero3, test

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -14,8 +14,37 @@ afterAll(() => {
 
 test('Se debería iniciar la aplicación sin películas', async () => {
     const URL = `${baseURL}/movies`;
-    const req = await fetch(URL)
-    const movies = await req.json()
+    const req = await fetch(URL);
+    const movies = await req.json();
+    expect(movies.length).toBe(0);
+})
+test('Se debería reflejar en la base de datos la pelicula agregada', async () =>
+{
+	const URL = `${baseURL}/movies`;
+	 const movie = {
+        name: 'troya',
+        plot: 'Pelicula de accion',
+        year: '2004',
+        country: 'EEUU',
+        runtime: '111',
+        language: 'England',
+        generes: 'accion',
+        writers: 'nose',
+        directors: 'nose'
+    }
+	function create(movie) {fetch(URL, {
+        method: 'post',
+       body:    JSON.stringify({title:movie.name,description:movie.plot,year:anio,
+       	runtime:movie.runtime,country:movie.country,language:movie.language,
+       	genres:movie.generes,writers:movie.writers,directors:movie.directors})
+    })	
+	}
 
-    expect(movies.length).toBe(0)
-});
+	const result= function getAll() {
+    return fetch('/api/v1/movies')
+        .then(result => result.json())
+		}
+
+  expect(movie.title).toBe(result.title);
+
+}) 
